4b0f55893ff78649702d030e09dc80668125fd6d
Osunkoya Jibreel <osunkoyajubril6000@gmail.com>
2024-09-05 10:18:32 +0100

    admin role

---
+++ app/(dashboard)/admin/admin-roles/page.tsx
@@ -0,0 +1,40 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+const page = () => {
+  return (
+    <section className="mx-9 mt-10 h-[869px] bg-white rounded-xl">
+      <div className="pl-20 h-20 flex items-center border-b-[1px] border-[#E0E0E0] relative">
+        <p className="text-xl font-medium text-[#1E1E1E]">Permission</p>
+        <div className="w-[103px] h-[3px] bg-[#194A7A] absolute bottom-0" />
+      </div>
+      <div className=" h-[749px] bg-[#FAFAFA] m-5">
+        <Tabs
+          defaultValue="account"
+          orientation="vertical"
+          className=""
+        >
+          <TabsList className="flex">
+            <TabsTrigger value="one">One</TabsTrigger>
+            <TabsTrigger value="two">Two</TabsTrigger>
+            <TabsTrigger value="three">Three</TabsTrigger>
+            <TabsTrigger value="four">Four</TabsTrigger>
+          </TabsList >
+          <TabsContent value="one" className="grid">
+            One changes to your account here.
+          </TabsContent>
+          <TabsContent value="two" className="grid">
+            Two changes to your account here.
+          </TabsContent>
+          <TabsContent value="three" className="grid">
+            Three changes to your account here.
+          </TabsContent>
+          <TabsContent value="four" className="grid">
+            Four changes to your account here.
+          </TabsContent>
+        </Tabs>
+      </div>
+    </section>
+  );
+};
+
+export default page;
