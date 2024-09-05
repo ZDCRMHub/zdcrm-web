13ecab80bde78d49bf3f1ef6bdf9952f5307e4a3
Oni Khalid Ayòbámi <onikhalidayo@gmail.com>
2024-09-05 23:27:44 +0100

    update for deployment - remove all build errors

--- app/(dashboard)/delivery/page.tsx
+++ app/(dashboard)/delivery/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Layout from '@/components/Layout';
 import {OrderTimeline} from '@/components/order/OrderTimeline';
 import React from 'react';
--- app/(dashboard)/orders/page.tsx
+++ app/(dashboard)/orders/page.tsx
@@ -1,11 +1,10 @@
-import Layout from '@/components/Layout';
 import React from 'react';
 
 const page = () => {
   return (
-    <Layout>
-      <p>hello</p>
-    </Layout>
+   <div>
+
+   </div>
   );
 };
 
