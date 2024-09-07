fddfe06d13c4c3d05aaaab9109e402e51f8851c4
Osunkoya Jibreel <osunkoyajubril6000@gmail.com>
2024-09-07 14:13:11 +0100

    employeesRole

---
+++ app/(dashboard)/admin/employees-role/page.tsx
@@ -0,0 +1,249 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import React from "react";
+import { CiSearch } from "react-icons/ci";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { MdOutlineModeEdit } from "react-icons/md";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import { IoChevronUp } from "react-icons/io5";
+
+const memberDetails = [
+  {
+    memberID: "MEM001",
+    name: "Akin Gold",
+    email: "akingold@gmail.com",
+    phoneNumber: "08012345678",
+    dateCreated: "01 Mar 2023",
+    //   role: "Credit Card",
+    action: "Active",
+  },
+  {
+    memberID: "MEM001",
+    name: "Akin Gold",
+    email: "akingold@gmail.com",
+    phoneNumber: "08012345678",
+    dateCreated: "01 Mar 2023",
+    //   role: "Credit Card",
+    action: "Active",
+  },
+  {
+    memberID: "MEM001",
+    name: "Akin Gold",
+    email: "akingold@gmail.com",
+    phoneNumber: "08012345678",
+    dateCreated: "01 Mar 2023",
+    //   role: "Credit Card",
+    action: "Active",
+  },
+  {
+    memberID: "MEM001",
+    name: "Akin Gold",
+    email: "akingold@gmail.com",
+    phoneNumber: "08012345678",
+    dateCreated: "01 Mar 2023",
+    //   role: "Credit Card",
+    action: "Active",
+  },
+  {
+    memberID: "MEM001",
+    name: "Akin Gold",
+    email: "akingold@gmail.com",
+    phoneNumber: "08012345678",
+    dateCreated: "01 Mar 2023",
+    //   role: "Credit Card",
+    action: "Active",
+  },
+  {
+    memberID: "MEM001",
+    name: "Akin Gold",
+    email: "akingold@gmail.com",
+    phoneNumber: "08012345678",
+    dateCreated: "01 Mar 2023",
+    //   role: "Credit Card",
+    action: "Active",
+  },
+  {
+    memberID: "MEM001",
+    name: "Akin Gold",
+    email: "akingold@gmail.com",
+    phoneNumber: "08012345678",
+    dateCreated: "01 Mar 2023",
+    //   role: "Credit Card",
+    action: "Active",
+  },
+  {
+    memberID: "MEM001",
+    name: "Akin Gold",
+    email: "akingold@gmail.com",
+    phoneNumber: "08012345678",
+    dateCreated: "01 Mar 2023",
+    //   role: "Credit Card",
+    action: "Active",
+  },
+];
+
+const page = () => {
+  return (
+    <section className="mt-7 pb-7 mx-10 rounded-xl bg-white border-[1px] border-[#0F172B1A] px-[118px] pt-[35px]">
+      <div className="flex justify-between items-end">
+        <div className="flex flex-col gap-2">
+          <h1 className="text-xl font-medium">Employee Management</h1>
+          <p>Manage your employees and their account permissions here.</p>
+        </div>
+        <div className="relative">
+          <Input
+            type="search"
+            placeholder="Search what you need"
+            className="h-14 w-[485px]"
+          />
+          <CiSearch
+            size={20}
+            color="#111827"
+            className="absolute top-[30%] right-[24px]"
+          />
+        </div>
+      </div>
+      <div className="mt-6 flex justify-between items-start">
+        <h2 className="text-2xl font-semibold">Team Members</h2>
+        <div className="flex gap-2">
+          <Button className="h-12 flex gap-4 bg-[#111827] rounded-[10px] text-sm px-6">
+            Save Changes
+          </Button>
+          <Button className="h-12 flex gap-4 bg-transparent text-sm px-6 text-[#111827] border border-solid rounded-[10px]">
+            Add Employee
+          </Button>
+        </div>
+      </div>{" "}
+      <Table>
+        {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[22.9%]">Name</TableHead>
+            <TableHead className="w-[22.9%]">Phone Number</TableHead>
+            <TableHead className="w-[12.8%]">Created Date</TableHead>
+            <TableHead className="">Role</TableHead>
+            <TableHead className="w-[22.9%] text-right">Action</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {memberDetails.map((member) => (
+            <TableRow key={member.memberID}>
+              <TableCell className="font-medium">
+                <div>
+                  <p className="font-medium">{member.name}</p>
+                  <p className="text-sm">{member.email}</p>
+                </div>
+              </TableCell>
+              <TableCell>{member.phoneNumber}</TableCell>
+              <TableCell>{member.dateCreated}</TableCell>
+              <TableCell className="text-right">
+                <Select>
+                  <SelectTrigger className="border-none">
+                    <SelectValue placeholder="Select Role" className="" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectItem value="branch-manager">
+                        Branch Manager
+                      </SelectItem>
+                      <SelectItem value="marketer">Marketer</SelectItem>
+                      <SelectItem value="admin">Admin</SelectItem>
+                      <SelectItem value="delivery-rep">Delivery rep</SelectItem>
+                      <SelectItem value="prod-team">Production Team</SelectItem>
+                      <SelectItem value="employee">Employee</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </TableCell>
+              <TableCell className="text-right flex gap-[10px]">
+                <Select>
+                  <SelectTrigger>
+                    <SelectValue
+                      placeholder="Select Action"
+                      className={
+                        member.action === "Active"
+                          ? "bg-[#E7F7EF] text-[#0CAF60] border-none"
+                          : "bg-[rgba(224,49,55,0.31)] text-[#E03137] border-none"
+                      }
+                    />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectItem className="" value="active">
+                        Active
+                      </SelectItem>
+                      <SelectItem value="deactive">Deactivate</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+                <div className="p-2 rounded-lg bg-[#2F78EE] flex items-center cursor-pointer">
+                  <MdOutlineModeEdit color="#fff" size={20} />
+                </div>
+                <div className="p-2 rounded-lg bg-[#E03137] flex items-center cursor-pointer">
+                  <RiDeleteBin6Line color="#fff" size={20} />
+                </div>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <div className="w-full flex justify-between items-center mt-6">
+        <div>
+          <Pagination className="flex justify-start">
+            <PaginationContent>
+              <PaginationItem>
+                <PaginationPrevious href="#" />
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink href="#" isActive>
+                  1
+                </PaginationLink>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationNext href="#" />
+              </PaginationItem>
+            </PaginationContent>
+          </Pagination>
+        </div>
+        <div className="flex gap-4 items-center">
+          <p className="text-xs text-[#687588] ">Showing 1 to 8 of 8 entries</p>
+          <Button className="flex gap-4 bg-transparent border border-solid border-[#F1F2F4] text-[#111827] rounded-[10px] text-sm px-[10px]">
+            Show 8
+            <IoChevronUp />
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default page;
