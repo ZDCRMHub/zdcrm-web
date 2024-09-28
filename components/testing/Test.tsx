24defa91dff9ac300595c47c74cf142c601930cc
iamswart <swartjide@gmail.com>
2024-09-28 20:02:11 +0100

    test

---
+++ components/testing/Test.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Navbar} from '@/components/layout/Navbar';
+import {Sidebar} from '@/components/layout/Sidebar';
+
+const Test = ({children}: {children: React.ReactNode}) => {
+  return (
+    <div className='flex h-screen overflow-hidden'>
+      <Sidebar />
+      <div className='flex flex-col flex-1 overflow-hidden'>
+        <Navbar />
+        <main className='flex-1 overflow-x-hidden overflow-y-auto bg-[#FAFAFA]'>
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Test;
